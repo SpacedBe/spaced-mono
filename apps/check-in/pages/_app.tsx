51471a667aa4211cb9f65c05a430c90b1ddf7fd9
Nena Penel <nenapenel@Nenas-MacBook-Pro.local>
2021-05-18 09:42:46 +0200

    added Chakra

--- apps/check-in/pages/_app.tsx
+++ apps/check-in/pages/_app.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { Button } from '@spaced/ui';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>Welcome to check-in!</title>
       </Head>
@@ -13,8 +15,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <main>
           <Component {...pageProps} />
         </main>
+        <Button content={'hello'} disabled={false} />
       </div>
-    </>
+    </ChakraProvider>
   );
 }
 
