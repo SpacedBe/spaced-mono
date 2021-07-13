893b7feaf54b59f241adea642b2cd5706940ef43
Simon Peters <simon@spaced.be>
2021-07-13 14:17:04 +0200

    update everything

--- apps/check-in/next.config.js
+++ apps/check-in/next.config.js
@@ -2,11 +2,6 @@
 const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
-  nx: {
-    distDir: '.dist',
-    target: 'serverless',
-    // Set this to false if you do not want to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: true,
-  },
+  distDir: '.dist',
+  target: 'serverless',
 });
